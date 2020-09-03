464ad583d203cadccf31bd63a7fa29b4bca5ca08
Julia Golovach <jenmorgan93@Julias-Air.home>
2020-09-03 18:03:09 +0200

    changed variable result

--- learn.js
+++ learn.js
@@ -1,11 +1,10 @@
 let a = prompt("Fill in the first number");
 let b = prompt("Fill in the second number");
-let result;
-if(a===""||b==="") {
+if(a === "" || b === "") {
     alert("One of the numbers is missing");
 }
 else {
-    result = ((Number(a) + Number(b)) < 4) ? 'Too little' : "Too much";
+    let result = ((Number(a) + Number(b)) < 4) ? 'Too little' : "Too much";
     alert(result);
 }
 
