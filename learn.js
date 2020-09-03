0ee93fb5e0a1db204763fd939daa5d7ef772da48
Julia Golovach <jenmorgan93@Julias-Air.home>
2020-09-03 17:14:45 +0200

    if's lesson

--- learn.js
+++ learn.js
@@ -1,1 +1,5 @@
-alert(prompt("Назовите ваше имя"));
+let a = prompt("Fill in the first number");
+let b = prompt("Fill in the second number");
+let result;
+result = ((Number(a) + Number(b)) < 4) ? 'Too little' : "Too much";
+alert(result);
