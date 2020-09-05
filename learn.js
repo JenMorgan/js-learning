278625b6b9a04d1304144ec4c2b57a3a024c8cb2
Julia Golovach <jenmorgan93@Julias-Air.home>
2020-09-05 15:53:51 +0200

    for cycle lesson

--- learn.js
+++ learn.js
@@ -1,6 +1,8 @@
-for (i = 2;i<=10;i++) {
-    if (i % 2 !== 0) continue;
-    alert(i);
+let n = Number(prompt("Please, fill in the number"));
+simple :
+for (let i = 2; i < n; i++) {
+    for (let a = 2; a < i; a++) {
+        if (i % a === 0) continue simple;
+    }
+    alert (i);
 }
-
-
