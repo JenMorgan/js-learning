fa0714f27954c107abf9a51887dac244604c14f3
Julia Golovach <jenmorgan93@Julias-Air.home>
2020-09-10 15:39:18 +0200

    tests and etc.

--- learn.js
+++ learn.js
@@ -1,14 +1,8 @@
-const person = prompt("Fill in your Login");
-
-if (person) {
-    if (person === "Admin") {
-        const password = prompt("Fill in your password");
-        password
-            ? alert(password === "Admin" ? "Hi!" : "Wrong password!")
-            : alert("Cancelled!");
-    } else {
-        alert("I don't know you");
+const equalDigit = (num1, num2, num3) => {
+    if (num1 < 0 || num2 < 0 || num3 < 0) { 
+      return false;
     }
-} else {
-    alert("Cancelled!");
-}
+    return num1 % 10 === num2 % 10 || 
+           num2 % 10 === num3 % 10 ||
+           num1 % 10 === num3 % 10;
+  }
---
+++ test.js
@@ -0,0 +1,6 @@
+describe ("equalDigit", function() {
+    it("возвращает true, если 2 или 3 числа имеют одинаковое самое правое число", function() {
+        assert.equal(equalDigit(24, 33, 544), true);
+    });
+});
+
